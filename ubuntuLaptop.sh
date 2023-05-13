052793b1e990d814ccb87be053eca77b3ff99372
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-05-12 18:56:41 -0700

    Update ubuntuLaptop.sh

--- ubuntuLaptop.sh
+++ ubuntuLaptop.sh
@@ -3,3 +3,8 @@ sudo apt upgrade -y
 sudo apt install ufw tlp preload
 sudo ufw enable
 sudo tlp start
+cd Downloads
+mkdir git
+wget https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-desktop-amd64.iso?_ga=2.175045409.245219162.1683942175-600468238.1683942175
+wget https://cdimage.kali.org/kali-2023.1/kali-linux-2023.1-live-amd64.iso
+
