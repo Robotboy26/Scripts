493eff2a334869cf728a34679de8c3f7f81fbe1f
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-05-12 19:31:03 -0700

    Update ubuntuLaptop.sh

--- ubuntuLaptop.sh
+++ ubuntuLaptop.sh
@@ -1,9 +1,15 @@
 sudo apt update -y
 sudo apt upgrade -y
+sudo apt install -y curl gpg
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt update
+sudo apt install -y code
 pactl set-sink-mute 0 1
 echo "set-sink-mute 0 1" | sudo tee -a /etc/pulse/default.pa
 sudo apt remove bluez -y
-sudo apt install ufw tlp preload pip -y
+sudo apt install ufw tlp preload python3-pip -y
 sudo apt autoremove -y
 sudo apt update -y
 sudo ufw enable
