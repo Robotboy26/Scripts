fd827997a5402b93cd5ee9d30920022eeba890d9
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-05-15 09:11:09 -0700

    Update DAGHR.sh

--- DAGHR.sh
+++ DAGHR.sh
@@ -1,14 +1,18 @@
 # Set your GitHub username and access token
 GH_USERNAME=<github-username>
-GH_TOKEN=<github-personal-access-token-with-repo-permissions>
+GH_TOKEN=<github-token>
 
 # Set the base URL for the GitHub API
 API_URL="https://api.github.com"
 
 # Get a list of all your repositories using the GitHub API
-REPO_LIST=$(curl -sSL -H "Authorization: token $GH_TOKEN" "$API_URL/users/$GH_USERNAME/repos?per_page=1000" | jq -r '.[].clone_url')
+REPO_LIST=$(curl -sSL -H "Authorization: token $GH_TOKEN" "$API_URL/user/repos?type=all&per_page=1000" | jq -r '.[].clone_url')
 
-# Loop through each repository and clone it
-for REPO_URL in $REPO_LIST; do
-    git clone $REPO_URL
+# Read the list of wanted repositories from the file
+WANTED_REPOS=$(cat wantedList.txt)
+
+# Loop through each repository in the list
+for REPO_URL in $WANTED_REPOS; do
+    # Clone the repository if it's in the list
+    git clone $REPO_URL || true
 done
