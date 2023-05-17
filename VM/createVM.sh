8cd20d486b92ff7cc4aaf9289ab0ba4852ad9364
robotboy26 <robotboygamer26@gmail.com>
2023-05-16 17:13:19 -0700

    Changes to be committed:

--- VM/createVM.sh
+++ VM/createVM.sh
@@ -1,10 +1,11 @@
-# Download the latest Ubuntu ISO
-if [ -f /home/robot/Desktop/ubuntu22.iso ]; then
-    echo "File exists"
-else
-    echo "File does not exist"
-    wget ubuntu22.iso https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-desktop-amd64.iso?_ga=2.175045409.245219162.1683942175-600468238.1683942175
-fi
+for file in "ubuntu22.iso" "kali.iso"; do
+    echo "Checking $file"
+    if [ -f "$file" ]; then
+        echo "File exists, skipping..."
+    else
+        echo "File does not exist, you need to download it (run setupVM.sh)"
+    fi
+done
 
 sudo apt-get update
 sudo apt-get install -y qemu-kvm qemu virt-manager virt-viewer libvirt-daemon libvirt-clients bridge-utils
--- ubuntuLaptop.sh
+++ ubuntuLaptop.sh
@@ -4,61 +4,63 @@ sudo apt install -y curl gpg
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-sudo apt update
+sudo apt update -y
 sudo apt install -y code
 pactl set-sink-mute 0 1
 echo "set-sink-mute 0 1" | sudo tee -a /etc/pulse/default.pa
 sudo apt remove bluez -y
-sudo apt install ufw tlp preload pip qemu qemu-systemx86_64 -y
+sudo apt install ufw tlp preload pip git qemu qemu-system-x86 steam -y
 sudo apt autoremove -y
 sudo apt update -y
 sudo ufw enable
 sudo tlp start
 cd Downloads
 
-echo "checking ubuntu22.iso"
-if [ -f "ubuntu22.iso" ]; then
-    echo "File exists skiping..."
-else
-    echo "File does not exist downloading..."
-    wget -O ubuntu22.iso https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-desktop-amd64.iso?_ga=2.175045409.245219162.1683942175-600468238.1683942175
-fi
+declare -A files=(
+    ["ubuntu22.iso"]="https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-desktop-amd64.iso?_ga=2.175045409.245219162.1683942175-600468238.1683942175"
+    ["kali.iso"]="https://cdimage.kali.org/kali-2023.1/kali-linux-2023.1-live-amd64.iso"
+    ["setupVM.sh"]="https://raw.githubusercontent.com/Robotboy26/Scripts/main/VM/setupVM.sh"
+    ["createVM.sh"]="https://raw.githubusercontent.com/Robotboy26/Scripts/main/VM/createVM.sh"
+    ["runVM.sh"]="https://raw.githubusercontent.com/Robotboy26/Scripts/main/VM/runVM.sh"
+    ["CGHRL.sh"]="https://raw.githubusercontent.com/Robotboy26/Scripts/main/github/CGHRL.sh"
+    ["DAGHR.sh"]="https://raw.githubusercontent.com/Robotboy26/Scripts/main/github/DAGHR.sh"
+    ["wanted.txt"]="https://raw.githubusercontent.com/Robotboy26/Scripts/main/github/wanted.txt"
+    ["requirements.txt"]="https://raw.githubusercontent.com/Robotboy26/Scripts/main/requirements.txt"
+)
 
-echo "checking kali.iso"
-if [ -f "kali.iso" ]; then
-    echo "File exists skiping..."
-else
-    echo "File does not exist downloading..."
-    wget -O kali.iso https://cdimage.kali.org/kali-2023.1/kali-linux-2023.1-live-amd64.iso
-fi
-
-mkdir VM
+mkdir -p VM
 cd VM
-echo "checking createVM.sh"
-if [ -f "createVM.sh" ]; then
-    echo "File exists skiping..."
-else
-    echo "File does not exist downloading..."
-    wget -O createVM.sh https://raw.githubusercontent.com/Robotboy26/Scripts/main/VM/createVM.sh
-fi
 
-echo "checking createVM.sh"
-if [ -f "runVM.sh" ]; then
-    echo "File exists skiping..."
-else
-    echo "File does not exist downloading..."
-    wget -O runVM.sh wget https://raw.githubusercontent.com/Robotboy26/Scripts/main/github/runVM.sh
-fi
+for file in "setupVM.sh" "createVM.sh" "runVM.sh"; do
+    echo "Checking $file"
+    if [ -f "$file" ]; then
+        echo "File exists, skipping..."
+    else
+        echo "File does not exist, downloading..."
+        wget -O "$file" "https://raw.githubusercontent.com/Robotboy26/Scripts/main/VM/$file"
+    fi
+done
+
+./setupVM.sh
 
 cd ..
 
-mkdir git
+mkdir -p git
 cd git
-wget https://raw.githubusercontent.com/Robotboy26/Scripts/main/github/CGHRL.sh
-wget https://raw.githubusercontent.com/Robotboy26/Scripts/main/github/DAGHR.sh
-wget https://raw.githubusercontent.com/Robotboy26/Scripts/main/github/wanted.txt
-wget https://raw.githubusercontent.com/Robotboy26/Scripts/main/requirements.txt
-sudo apt install meson # this is needed for d-bus python ; in might not be needed
-pip install -r requirements.txt
-rm requirements.txt
 
+for file in "CGHRL.sh" "DAGHR.sh" "wanted.txt" "requirements.txt"; do
+    echo "Checking $file"
+    if [ -f "$file" ]; then
+        echo "File exists, skipping..."
+    else
+        echo "File does not exist, downloading..."
+        wget -O "$file" "https://raw.githubusercontent.com/Robotboy26/Scripts/main/github/$file"
+    fi
+done
+
+sudo apt install meson # this is needed for d-bus python ; in might not be needed
+python3 -m pip install -r requirements.txt
+rm requirements.txt
+steam
+
+# todo move iso download and check to createVM.sh
