136e92ead56a3d6435f65ac29491286582581ae5
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-05-17 12:29:20 -0700

    Update ubuntuLaptop.sh
    remove pip install build and twine because i am adding them to requirements.txt

--- ubuntuLaptop.sh
+++ ubuntuLaptop.sh
@@ -60,7 +60,5 @@ done
 
 sudo apt install meson # this is needed for d-bus python ; in might not be needed
 python3 -m pip install -r requirements.txt
-pip install build
-pip install twine
 rm requirements.txt
 steam
