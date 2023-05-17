373ffa670a1b3ee6cffa865ab09a2d482beecf68
robotboy26 <robotboygamer26@gmail.com>
2023-05-17 12:09:16 -0700

    Changes to be committed:

--- ubuntuLaptop.sh
+++ ubuntuLaptop.sh
@@ -9,7 +9,8 @@ sudo apt install -y code
 pactl set-sink-mute 0 1
 echo "set-sink-mute 0 1" | sudo tee -a /etc/pulse/default.pa
 sudo apt remove bluez -y
-sudo apt install ufw tlp preload python3-pip qemu qemu-systemx86_64 -y
+sudo apt install ufw tlp preload python3-pip -y
+sudo apt install qemu qemu-systemx86_64 nasm xterm -y
 sudo apt autoremove -y
 sudo apt update -y
 sudo ufw enable
