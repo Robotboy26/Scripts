e0deb1c2dda36f1bf04753713f97e8c4aa37e327
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-05-27 10:54:21 -0700

    Update preRun.sh

--- preRun.sh
+++ preRun.sh
@@ -8,16 +8,20 @@ devpi_server_url="http://localhost:3141"
 # Configure Apt to use the Apt caching server
 sudo echo "Acquire::http::Proxy \"http://$apt_server_ip:3142\";" | sudo tee /etc/apt/apt.conf.d/02proxy
 
-# Configure the Devpi client to use the Devpi server
+# Install and configure Devpi client
 pip install --user devpi-client
 
 # Set the full path to the devpi command
 devpi_path="$HOME/.local/bin/devpi"
 
-# Use the Devpi server
-"$devpi_path" use "$devpi_server_url"
+# Authenticate with the Devpi server
 "$devpi_path" login root --password='admin'
-"$devpi_path" index --set-cfg-attr client/index-url "$devpi_server_url/root/pypi/+simple/"
+
+# Create a new index on the Devpi server
+"$devpi_path" index -c myindex bases=root/pypi
+
+# Set the default index to the newly created index
+"$devpi_path" use "$devpi_server_url/root/myindex"
 
 # Display the configured server IP and URL
 echo "Your Apt caching server IP: $apt_server_ip"
