31eb4b498decf0683edbcc9ef26014d26aebe4e6
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-05-27 11:16:06 -0700

    Update preRun.sh

--- preRun.sh
+++ preRun.sh
@@ -1,9 +1,16 @@
-sudo apt install pip
-# Set the IP address of the Apt caching server
-apt_server_ip="192.168.1.65"
-# Configure Apt to use the Apt caching server
-sudo echo "Acquire::http::Proxy \"http://$apt_server_ip:3142\";" | sudo tee /etc/apt/apt.conf.d/02proxy
+# Set the IP address of the caching server
+caching_server_ip="192.168.1.65"
 
-# Display the configured server IP and URL
-echo "Your Apt caching server IP: $apt_server_ip"
+# Configure APT to use the caching server
+apt_proxy_conf="/etc/apt/apt.conf.d/01proxy"
+echo "Acquire::http::Proxy \"http://$caching_server_ip:3142\";" | sudo tee $apt_proxy_conf
+echo "Acquire::https::Proxy \"false\";" | sudo tee -a $apt_proxy_conf
 
+# Configure pip to use the caching server
+pip_conf="$HOME/.pip/pip.conf"
+mkdir -p "$HOME/.pip"
+echo "[global]" >> $pip_conf
+echo "index-url = http://$caching_server_ip:3141/root/pypi/+simple/" >> $pip_conf
+
+# Display the configured server IP
+echo "Your caching server IP: $caching_server_ip"
