26fdd02a33c9a12d9e6d3f7bbd9cbd2495207866
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-05-27 11:17:40 -0700

    Update createVM.sh

--- VM/createVM.sh
+++ VM/createVM.sh
@@ -7,5 +7,5 @@ for file in "ubuntu22.iso" "kali.iso"; do
     fi
 done
 
-qemu-img create -f qcow2 ubuntu_disk.qcow2 20G
+qemu-img create -f qcow2 ubuntu_disk.qcow2 40G
 qemu-system-x86_64 -m 12256 -boot d -cdrom ubuntu22.iso -drive file=ubuntu_disk.qcow2,format=qcow2 -enable-kvm
